d467a30f762dec2fd4be91bebf299c178e3c67dd
Yatin Jain <rokstaryatin2012@gmail.com>
2021-02-06 22:40:06 +0530

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# MovieFightApp
+## MovieFightApp
  Simple JS movie comparison app.
+
+# Link
+    [Movie Fight App](theyatinjain.github.io/MovieFightApp)
---
+++ utils.js
@@ -0,0 +1,9 @@
+const debounce = (func, delay=1000) => {
+    let timeoutId;
+    return (...args) => {
+        if(timeoutId) clearTimeout(timeoutId);
+        timeoutId = setTimeout(() =>{
+            func.apply(null, args);
+        }, delay);
+    };
+};
