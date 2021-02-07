de153841b821d99cf2f9e60f4bc3d1217c78389f
Yatin Jain <rokstaryatin2012@gmail.com>
2021-02-08 00:17:01 +0530

    minor fix :)

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
         params: {
             apikey: 'bfa4358a',
             i: movie.imdbID
